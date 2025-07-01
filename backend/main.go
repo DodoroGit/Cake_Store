4e94bf9de6406b37221d545c24de485f7372efd5
Rick-Wj <dokebi871218@gmail.com>
2025-07-01 13:19:03 +0800

    feat(2025/7/1): push to aws ec2

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"log"
+
 	"github.com/DodoroGit/Cake_Store/database"
 	"github.com/DodoroGit/Cake_Store/graph"
 	"github.com/DodoroGit/Cake_Store/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// 載入 .env 環境變數
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("❌ 無法讀取 .env 檔案")
+	}
+
 	r := gin.Default()
+
+	// 初始化資料庫
 	database.InitDB()
 
 	// 初始化 GraphQL schema
@@ -17,5 +28,6 @@ func main() {
 	// 註冊 GraphQL endpoint
 	r.Any("/graphql", routes.GraphqlHandler())
 
-	r.Run(":8080") // 啟動在 localhost:8080
+	// 啟動 server
+	r.Run(":8080")
 }
