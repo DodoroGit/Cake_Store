9ebba4cb0380b96738f0cd9cfb49dc165299b71c
Rick-Wj <dokebi871218@gmail.com>
2025-07-09 02:31:58 +0800

    feat(2025/7/9): modify backend middleware and graphql route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,31 +1,19 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/DodoroGit/Cake_Store/graph"
 	"github.com/gin-gonic/gin"
-	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
-type GraphQLRequest struct {
-	Query string `json:"query"`
-}
-
 func GraphqlHandler() gin.HandlerFunc {
+	h := handler.New(&handler.Config{
+		Schema:   &graph.Schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
 	return func(c *gin.Context) {
-		var req GraphQLRequest
-		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "無效的請求"})
-			return
-		}
-
-		result := graphql.Do(graphql.Params{
-			Schema:        graph.Schema,
-			RequestString: req.Query,
-			Context:       c.Request.Context(), // ✅ 重點：帶入 Middleware 設定的 context
-		})
-
-		c.JSON(http.StatusOK, result)
+		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
