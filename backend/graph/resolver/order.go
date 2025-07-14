205cb1b5b05e0c4acc40ff51ccf450d1fb9576a7
Rick-Wj <dokebi871218@gmail.com>
2025-07-14 22:03:03 +0800

    feat(2025/7/14): add new fucntion for member page

--- backend/graph/resolver/order.go
+++ backend/graph/resolver/order.go
@@ -14,11 +14,15 @@ var OrderItemType = graphql.NewObject(graphql.ObjectConfig{
 var OrderType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Order",
 	Fields: graphql.Fields{
-		"id":          &graphql.Field{Type: graphql.Int},
-		"createdAt":   &graphql.Field{Type: graphql.String},
-		"status":      &graphql.Field{Type: graphql.String},
-		"items":       &graphql.Field{Type: graphql.NewList(OrderItemType)},
-		"totalAmount": &graphql.Field{Type: graphql.Float},
-		"pickupDate":  &graphql.Field{Type: graphql.String},
+		"id":           &graphql.Field{Type: graphql.Int},
+		"createdAt":    &graphql.Field{Type: graphql.String},
+		"status":       &graphql.Field{Type: graphql.String},
+		"items":        &graphql.Field{Type: graphql.NewList(OrderItemType)},
+		"totalAmount":  &graphql.Field{Type: graphql.Float},
+		"pickupDate":   &graphql.Field{Type: graphql.String},
+		"orderNumber":  &graphql.Field{Type: graphql.String},
+		"pickupMethod": &graphql.Field{Type: graphql.String},
+		"address":      &graphql.Field{Type: graphql.String},
+		"pickupTime":   &graphql.Field{Type: graphql.String},
 	},
 })
